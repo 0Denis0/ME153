b40efa912f2faff3758ab1d09dab2bb8bd065614
Christopher Lew <69989812+Chris-12345@users.noreply.github.com>
2024-05-27 18:26:29 -0700

    calcMotorSpeed_PID works. You can copy paste the whole function into your code
    PID gains still need to be tuned. An external program must give the the desired location and keep track of derivative and integral error

--- Motor_PID_Control.py
+++ Motor_PID_Control.py
@@ -10,18 +10,29 @@ import math
 
 
 # Constant Parameters: 
-TRACKING_TOLERANCE = 0.1; 
-kp = 8 #proportional constant
-ki = 0 #integralError constant
-kd = 0 #derivativeError constant
-ANGLE_OF_GOING_RIGHT = -90; #angle (deg) the robot detects when moving right. Set to -90 if up is 0, CCW is positive.
+# TRACKING_TOLERANCE = 0.1; 
+# kp = 8 #proportional constant
+# ki = 0 #integralError constant
+# kd = 0 #derivativeError constant
+# ANGLE_OF_GOING_RIGHT = -90; #angle (deg) the robot detects when moving right. Set to -90 if up is 0, CCW is positive.
 
 
-DEFAULT_SPEED = 255 #the regular speed the robot runs at
-MAX_SPEED = 255 #exactly 255
-CCW_ANGLE_DIRECTION = 1; #set to 1 if CCW is negative
+# DEFAULT_SPEED = 255 #the regular speed the robot runs at
+# MAX_SPEED = 255 #exactly 255
+# CCW_ANGLE_DIRECTION = 1; #set to 1 if CCW is negative
 
-def calcMotorSpeed_PIDControl (x, y, phi, x_desired, y_desired, integralError, derivativeError):
+#phi is the raw angle measurement (deg)
+def calcMotorSpeed_PID(x, y, phi, x_desired, y_desired, integralError, derivativeError):
+    # Constant Parameters: 
+    TRACKING_TOLERANCE = 0.1; #an outside program needs to change the (x_desired,y_desired) 
+    kp = 8 #proportional constant
+    ki = 0 #integralError constant
+    kd = 0 #derivativeError constant
+    ANGLE_OF_GOING_RIGHT = -90; #angle (deg) the robot detects when moving right. Set to -90 if up is 0, CCW is positive.
+    DEFAULT_SPEED = 255 #the regular speed the robot runs at
+    MAX_SPEED = 255 #exactly 255
+    CCW_ANGLE_DIRECTION = 1; #set to 1 if CCW is negative
+    ######
     leftSpeed = DEFAULT_SPEED
     rightSpeed = DEFAULT_SPEED
     dx = x_desired - x;
@@ -41,14 +52,20 @@ def calcMotorSpeed_PIDControl (x, y, phi, x_desired, y_desired, integralError, d
 
 
 
-# Unused Helper Function:
-def calculate_angle_to_desired(x, y, x_desired, y_desired):
-    angleToDesired =  math.atan2(y_desired - y, x_desired - x);
-    return angleToDesired
+# # Unused Helper Function:
+# def calculate_angle_to_desired(x, y, x_desired, y_desired):
+#     angleToDesired =  math.atan2(y_desired - y, x_desired - x);
+#     return angleToDesired
 
-# Unused Helper Function, calculate u for 
-def calculateControlOutput_u(error, integralError, derivativeError):
-    integralError += error
-    output = kp * error + ki * integralError + kd * derivativeError
-    #self.prev_error = error
-    return output
+# # Unused Helper Function, calculate u for 
+# def calculateControlOutput_u(error, integralError, derivativeError):
+#     integralError += error
+#     output = kp * error + ki * integralError + kd * derivativeError
+#     #self.prev_error = error
+#     return output
+
+
+
+
+
+print(calcMotorSpeed_PID(0, 0, -90, 10, 0, 0, 0));
